161d4a9f1ed4701f6c04c99b791b68ec0ae9a9f5
Dreamhacker-Fear <lucaebraeuer@gmail.com>
2026-08-21 03:15:10 +0200

    Integrate storage for indicator settings

--- plugins/template/src/Settings.tsx
+++ plugins/template/src/Settings.tsx
@@ -1,23 +1,26 @@
 import { Forms } from "@vendetta/ui/components";
+import { storage } from "@vendetta/plugin";
 
 const { FormSection, FormSwitchRow } = Forms;
 
 export default () => (
-    <>
-        <FormSection title="Indicator Settings">
-            <FormSwitchRow
-                label="Hide white unread dots"
-                subLabel="Hide unread indicators."
-                value={true}
-                onValueChange={() => {}}
-            />
+    <FormSection title="Indicator Settings">
+        <FormSwitchRow
+            label="Hide white unread dots"
+            subLabel="Hide unread indicators."
+            value={storage.hideUnreadDots ?? true}
+            onValueChange={(value) => {
+                storage.hideUnreadDots = value;
+            }}
+        />
 
-            <FormSwitchRow
-                label="Hide @ mention indicators"
-                subLabel="Hide mention indicators."
-                value={true}
-                onValueChange={() => {}}
-            />
-        </FormSection>
-    </>
+        <FormSwitchRow
+            label="Hide @ mention indicators"
+            subLabel="Hide mention indicators."
+            value={storage.hideMentionIndicators ?? true}
+            onValueChange={(value) => {
+                storage.hideMentionIndicators = value;
+            }}
+        />
+    </FormSection>
 );
