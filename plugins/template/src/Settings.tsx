e3b57c3a1937d7c43e4bba570bbdc909ec08b91c
Dreamhacker-Fear <lucaebraeuer@gmail.com>
2026-08-21 03:04:33 +0200

    Implement indicator settings in Settings component
    Added settings for hiding unread dots and mention indicators.

--- plugins/template/src/Settings.tsx
+++ plugins/template/src/Settings.tsx
@@ -1,8 +1,38 @@
+import { ReactNative as RN, stylesheet } from "@vendetta/metro/common";
 import { Forms } from "@vendetta/ui/components";
-const { FormText } = Forms;
+import { React } from "@vendetta/metro/common";
 
-export default () => (
-    <FormText>
-        Hello, world!
-    </FormText>
-)
+const { FormSection, FormRow, FormSwitch } = Forms;
+
+export default () => {
+    const [hideUnreadDots, setHideUnreadDots] = React.useState(true);
+    const [hideMentionIndicators, setHideMentionIndicators] = React.useState(true);
+
+    return (
+        <RN.ScrollView>
+            <FormSection title="Indicator Settings">
+                <FormRow
+                    label="Hide white unread dots"
+                    subLabel="Hide the normal unread indicators."
+                    trailing={
+                        <FormSwitch
+                            value={hideUnreadDots}
+                            onValueChange={setHideUnreadDots}
+                        />
+                    }
+                />
+
+                <FormRow
+                    label="Hide @ mention indicators"
+                    subLabel="Hide indicators caused by mentions."
+                    trailing={
+                        <FormSwitch
+                            value={hideMentionIndicators}
+                            onValueChange={setHideMentionIndicators}
+                        />
+                    }
+                />
+            </FormSection>
+        </RN.ScrollView>
+    );
+};
