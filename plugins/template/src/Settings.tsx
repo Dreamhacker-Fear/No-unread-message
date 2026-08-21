2d572a56d51738546ef8fcf1d89f78403c4fae9c
Dreamhacker-Fear <lucaebraeuer@gmail.com>
2026-08-21 03:12:22 +0200

    Add option to hide @ mention indicators in settings

--- plugins/template/src/Settings.tsx
+++ plugins/template/src/Settings.tsx
@@ -3,12 +3,21 @@ import { Forms } from "@vendetta/ui/components";
 const { FormSection, FormSwitchRow } = Forms;
 
 export default () => (
-    <FormSection title="Indicator Settings">
-        <FormSwitchRow
-            label="Hide white unread dots"
-            subLabel="Hide unread indicators."
-            value={true}
-            onValueChange={() => {}}
-        />
-    </FormSection>
+    <>
+        <FormSection title="Indicator Settings">
+            <FormSwitchRow
+                label="Hide white unread dots"
+                subLabel="Hide unread indicators."
+                value={true}
+                onValueChange={() => {}}
+            />
+
+            <FormSwitchRow
+                label="Hide @ mention indicators"
+                subLabel="Hide mention indicators."
+                value={true}
+                onValueChange={() => {}}
+            />
+        </FormSection>
+    </>
 );
