3320936653dfa60c0eff4eb639f3054a32ae89ff
Dreamhacker-Fear <lucaebraeuer@gmail.com>
2026-08-21 10:58:01 +0200

    Update Settings component text

--- plugins/template/src/Settings.tsx
+++ plugins/template/src/Settings.tsx
@@ -4,8 +4,6 @@ const { FormText } = Forms;
 
 export default () => (
     <FormText>
-        No Unread Dots{"\n\n"}
-        Messages cleared: 0{"\n\n"}
-        The counter will be connected to the read system next.
+        No Unread Dots Settings
     </FormText>
 );
