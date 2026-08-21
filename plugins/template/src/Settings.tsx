3c44806ca4fa7a79467e14d587de366668bc1de7
Dreamhacker-Fear <lucaebraeuer@gmail.com>
2026-08-21 03:07:01 +0200

    Simplify Settings component to display message
    Removed unread dots settings and replaced with a message.

--- plugins/template/src/Settings.tsx
+++ plugins/template/src/Settings.tsx
@@ -1,38 +1,9 @@
-import { ReactNative as RN, stylesheet } from "@vendetta/metro/common";
 import { Forms } from "@vendetta/ui/components";
-import { React } from "@vendetta/metro/common";
 
-const { FormSection, FormRow, FormSwitch } = Forms;
+const { FormText } = Forms;
 
-export default () => {
-    const [hideUnreadDots, setHideUnreadDots] = React.useState(true);
-    const [hideMentionIndicators, setHideMentionIndicators] = React.useState(true);
-
-    return (
-        <RN.ScrollView>
-            <FormSection title="Indicator Settings">
-                <FormRow
-                    label="Hide white unread dots"
-                    subLabel="Hide the normal unread indicators."
-                    trailing={
-                        <FormSwitch
-                            value={hideUnreadDots}
-                            onValueChange={setHideUnreadDots}
-                        />
-                    }
-                />
-
-                <FormRow
-                    label="Hide @ mention indicators"
-                    subLabel="Hide indicators caused by mentions."
-                    trailing={
-                        <FormSwitch
-                            value={hideMentionIndicators}
-                            onValueChange={setHideMentionIndicators}
-                        />
-                    }
-                />
-            </FormSection>
-        </RN.ScrollView>
-    );
-};
+export default () => (
+    <FormText>
+        No Unread Dots Settings
+    </FormText>
+);
