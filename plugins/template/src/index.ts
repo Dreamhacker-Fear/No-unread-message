17375f659ddabbfb00519a136ff374f92a9461a9
Dreamhacker-Fear <lucaebraeuer@gmail.com>
2026-08-21 03:10:31 +0200

    Refactor logging in onLoad and onUnload methods

--- plugins/template/src/index.ts
+++ plugins/template/src/index.ts
@@ -1,36 +1,13 @@
-import { logger, metro } from "@vendetta";
+import { logger } from "@vendetta";
 import Settings from "./Settings";
 
 export default {
     onLoad: () => {
-        logger.log("No Unread Dots: loaded");
-
-        try {
-            const modules = metro.findAll(
-                (m: any) =>
-                    m &&
-                    typeof m === "object" &&
-                    (
-                        typeof m.getUnreadCount === "function" ||
-                        typeof m.hasUnread === "function" ||
-                        typeof m.ackMessageId === "function"
-                    )
-            );
-
-            logger.log(
-                `No Unread Dots: found ${modules.length} read-state module(s)`
-            );
-
-            for (const module of modules) {
-                logger.log(module);
-            }
-        } catch (e) {
-            logger.error(`No Unread Dots: ${e}`);
-        }
+        logger.log("No Unread Dots loaded");
     },
 
     onUnload: () => {
-        logger.log("No Unread Dots: unloaded");
+        logger.log("No Unread Dots unloaded");
     },
 
     settings: Settings,
