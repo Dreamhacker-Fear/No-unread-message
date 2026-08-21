6f0695ebf928cc1eaba4dd964d8bf3e57896798b
Dreamhacker-Fear <lucaebraeuer@gmail.com>
2026-08-21 03:20:48 +0200

    Simplify settings component to display message
    Removed unread dots settings and replaced with a message.

--- plugins/template/src/Settings.tsx
+++ plugins/template/src/Settings.tsx
@@ -1,33 +1,9 @@
 import { Forms } from "@vendetta/ui/components";
-import { storage } from "@vendetta/plugin";
-import { React } from "@vendetta/metro/common";
 
-const { FormSection, FormSwitchRow } = Forms;
+const { FormText } = Forms;
 
-export default () => {
-    const [, forceUpdate] = React.useState(0);
-
-    return (
-        <FormSection title="Indicator Settings">
-            <FormSwitchRow
-                label="Hide white unread dots"
-                subLabel="Hide unread indicators."
-                value={storage.hideUnreadDots ?? true}
-                onValueChange={(value) => {
-                    storage.hideUnreadDots = value;
-                    forceUpdate((n) => n + 1);
-                }}
-            />
-
-            <FormSwitchRow
-                label="Hide @ mention indicators"
-                subLabel="Hide mention indicators."
-                value={storage.hideMentionIndicators ?? true}
-                onValueChange={(value) => {
-                    storage.hideMentionIndicators = value;
-                    forceUpdate((n) => n + 1);
-                }}
-            />
-        </FormSection>
-    );
-};
+export default () => (
+    <FormText>
+        No Unread Dots Settings
+    </FormText>
+);
